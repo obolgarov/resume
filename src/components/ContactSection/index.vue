a79d01e2540f59c4ab48d62255481fc8d802abc2
Oleksandr Bolgarov <oleksandr@bolgarov.org>
2018-03-02 20:49:59 -0500

    fix model error

--- src/components/ContactSection/index.vue
+++ src/components/ContactSection/index.vue
@@ -2,9 +2,9 @@
   <div class="contactSection">
     <SectionCard icon="send" title="Contact">
       <FormComponent @submit="onSubmit">
-        <FormInput type="text" label="Name" :model="name"></FormInput>
-        <FormInput type="text" label="Email" :model="email"></FormInput>
-        <FormInput type="textarea" label="Message" :model="message"></FormInput>
+        <FormInput type="text" label="Name" v-model="name"></FormInput>
+        <FormInput type="text" label="Email" v-model="email"></FormInput>
+        <FormInput type="textarea" label="Message" v-model="message"></FormInput>
       </FormComponent>
     </SectionCard>
   </div>
@@ -37,13 +37,18 @@ export default {
   },
   methods: {
     onSubmit (recaptchaChallenge) {
-      // get ip of user before sending to server
-
+      console.log('submitting with challenge: ');
+      console.log(recaptchaChallenge);
       axios.post('https://api.resume.bolgarov.org/email', {
         name: this.name,
         email: this.email,
         message: this.message,
         recaptchaChallenge: recaptchaChallenge
+      }).then((result) => {
+        console.log('success');
+      }).catch((error) => {
+        console.log('error');
+        console.log(error);
       });
     }
   }
